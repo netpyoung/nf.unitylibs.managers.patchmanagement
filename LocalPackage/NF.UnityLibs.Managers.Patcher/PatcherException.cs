dbac5018bde6e47f7aac18d76e909ab8d1458400
netpyoung <netpyoung@gmail.com>
2024-11-06 15:06:40 +0900

    add: LocalPackage/NF.UnityLibs.Managers.Patcher/PatcherException.cs

---
+++ LocalPackage/NF.UnityLibs.Managers.Patcher/PatcherException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace NF.UnityLibs.Managers.Patcher
+{
+	public class PatcherException : Exception
+    {
+        public override string StackTrace { get; }
+
+        public PatcherException(string msg) : base(msg)
+        {
+            string st = Environment.StackTrace;
+            StackTrace = st.Substring(st.IndexOf('\n', st.IndexOf('\n') + 1) + 1);
+        }
+    }
+}
