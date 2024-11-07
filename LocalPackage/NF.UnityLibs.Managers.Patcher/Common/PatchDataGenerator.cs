dbc783352ffedab84301030ba49f9d9802c60350
netpyoung <netpyoung@gmail.com>
2024-11-07 11:12:32 +0900

    chore: fomatting

--- LocalPackage/NF.UnityLibs.Managers.Patcher/Common/PatchDataGenerator.cs
+++ LocalPackage/NF.UnityLibs.Managers.Patcher/Common/PatchDataGenerator.cs
@@ -7,88 +7,85 @@ using System.Threading.Tasks;
 
 namespace NF.UnityLibs.Managers.Patcher.Common
 {
-	public static class PatchDataGenerator
-	{
-		private static readonly HashSet<string> _DEFAULT_FILTER_SET = new HashSet<string>
-		{
-			$"{nameof(PatchFileList)}.json".ToLower(),
-			"buildlogtep.json".ToLower(),
-			"Android_manifest.manifest".ToLower(),
-			"iOS_manifest.manifest".ToLower(),
-		};
+    public static class PatchDataGenerator
+    {
+        private static readonly HashSet<string> _DEFAULT_FILTER_SET = new HashSet<string>
+        {
+            $"{nameof(PatchFileList)}.json".ToLower(),
+            "buildlogtep.json".ToLower(),
+            "Android_manifest.manifest".ToLower(),
+            "iOS_manifest.manifest".ToLower(),
+        };
 
-		public static string CreateDummyPatchVersionJson(string destDir)
-		{
-			PatchVersion x = PatchVersion.Dummy();
-			string json = x.ToJson();
-			File.WriteAllText($"{destDir}/{nameof(PatchVersion)}.json", json);
-			return json;
-		}
+        public static string CreateDummyPatchVersionJson(string destDir)
+        {
+            PatchVersion x = new PatchVersion();
+            x["latest"] = 0;
+            string json = x.ToJson();
+            File.WriteAllText($"{destDir}/{nameof(PatchVersion)}.json", json);
+            return json;
+        }
 
-		public static string? CreatePatchFileListJson(int patchNumber, string patchSrcDir, HashSet<string>? filterOrNull = null)
-		{
-			if (string.IsNullOrEmpty(patchSrcDir))
-			{
+        public static string? CreatePatchFileListJson(int patchNumber, string patchSrcDir, HashSet<string>? filterOrNull = null)
+        {
+            if (string.IsNullOrEmpty(patchSrcDir))
+            {
 #if UNITY_5_3_OR_NEWER
                 UnityEngine.Debug.LogError($"string.IsNullOrEmpty - patchSrcDir: {patchSrcDir}");
 #endif // UNITY_5_3_OR_NEWER
-				return null;
-			}
+                return null;
+            }
 
-			if (!Directory.Exists(patchSrcDir))
-			{
+            if (!Directory.Exists(patchSrcDir))
+            {
 #if UNITY_5_3_OR_NEWER
                 UnityEngine.Debug.LogError($"!Directory.Exists - patchSrcDir: {patchSrcDir}");
 #endif // UNITY_5_3_OR_NEWER
-				return null;
-			}
+                return null;
+            }
 
-			HashSet<string> filter;
-			if (filterOrNull == null)
-			{
-				filter = _DEFAULT_FILTER_SET;
-			}
-			else
-			{
-				filter = filterOrNull!;
-			}
+            HashSet<string> filter;
+            if (filterOrNull == null)
+            {
+                filter = _DEFAULT_FILTER_SET;
+            }
+            else
+            {
+                filter = filterOrNull!;
+            }
 
-			ConcurrentBag<PatchFileList.PatchFileInfo> cb = new ConcurrentBag<PatchFileList.PatchFileInfo>();
-			string[] paths = Directory.GetFiles(patchSrcDir, "*", SearchOption.TopDirectoryOnly);
-			List<Task> tasks = new List<Task>(paths.Length);
-			foreach (string path in paths.Select(x => x.Replace('\\', '/')))
-			{
-				string filename = Path.GetFileName(path).ToLower();
-				if (filter.Contains(filename))
-				{
-					continue;
-				}
+            ConcurrentQueue<PatchFileList.PatchFileInfo> cq = new ConcurrentQueue<PatchFileList.PatchFileInfo>();
+            string[] paths = Directory.GetFiles(patchSrcDir, "*", SearchOption.TopDirectoryOnly);
+            List<Task> tasks = new List<Task>(paths.Length);
+            foreach (string path in paths.Select(x => x.Replace('\\', '/')))
+            {
+                string filename = Path.GetFileName(path).ToLower();
+                if (filter.Contains(filename))
+                {
+                    continue;
+                }
 
-				long bytes = new FileInfo(path).Length;
-				tasks.Add(Task.Run(() =>
-				{
-					using (MemoryMappedFile mmf = MemoryMappedFile.CreateFromFile(path, FileMode.Open, null, bytes))
-					using (MemoryMappedViewStream stream = mmf.CreateViewStream(0, bytes, MemoryMappedFileAccess.Read))
-					{
-						uint checksum = CRC32.Compute(stream);
-						PatchFileList.PatchFileInfo patchFileInfo = new PatchFileList.PatchFileInfo(filename, bytes, checksum);
-						cb.Add(patchFileInfo);
-					}
-				}));
-			}
+                long bytes = new FileInfo(path).Length;
+                tasks.Add(Task.Run(() =>
+                {
+                    uint checksum = CRC32.ComputeFromFpath(path);
+                    PatchFileList.PatchFileInfo patchFileInfo = new PatchFileList.PatchFileInfo(filename, bytes, checksum);
+                    cq.Enqueue(patchFileInfo);
+                }));
+            }
 
-			Task.WhenAll(tasks).Wait();
-			Dictionary<string, PatchFileList.PatchFileInfo> dic = cb.ToDictionary(x => x.Name, x => x);
-			long totalBytes = dic.Sum(x => x.Value.Bytes);
-			PatchFileList patchFileList = new PatchFileList(patchNumber, dic, totalBytes);
-			string json = patchFileList.ToJson();
+            Task.WhenAll(tasks).Wait();
+            Dictionary<string, PatchFileList.PatchFileInfo> dic = cq.ToDictionary(x => x.Name, x => x);
+            long totalBytes = dic.Sum(x => x.Value.Bytes);
+            PatchFileList patchFileList = new PatchFileList(patchNumber, dic, totalBytes);
+            string json = patchFileList.ToJson();
 
-			string outputJsonFpath = $"{patchSrcDir}/{nameof(PatchFileList)}.json";
-			string? directoryPath = Path.GetDirectoryName(outputJsonFpath)!;
-			Directory.CreateDirectory(directoryPath);
+            string outputJsonFpath = $"{patchSrcDir}/{nameof(PatchFileList)}.json";
+            string? directoryPath = Path.GetDirectoryName(outputJsonFpath)!;
+            Directory.CreateDirectory(directoryPath);
 
-			File.WriteAllText(outputJsonFpath, json);
-			return json;
-		}
-	}
+            File.WriteAllText(outputJsonFpath, json);
+            return json;
+        }
+    }
 }
